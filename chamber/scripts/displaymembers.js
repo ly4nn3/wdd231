a0d65e098e2b73440c234051ea91a8bd350730ec
ly4nn3 <july4nn3@gmail.com>
2025-02-01 22:39:24 +0800

    Code cleanup

--- chamber/scripts/displaymembers.js
+++ chamber/scripts/displaymembers.js
@@ -135,23 +135,3 @@ listBtn.addEventListener('click', () => {
 });
 
 getMembersData();
-
-JAVASCRIPT
-
-Collapse
-// Hamburger menu functionality
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
-
-// Close menu when clicking a link
-document.querySelectorAll('.nav-menu a').forEach(link => {
-    link.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    });
-});
